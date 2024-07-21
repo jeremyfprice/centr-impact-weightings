509a699a9e6ed1f8a79407da4f2574337210a242
Jeremy Price <jfprice@iupui.edu>
2024-07-21 15:45:06 -0400

    set to download data from osf

--- process_weights.R
+++ process_weights.R
@@ -1,8 +1,10 @@
+library(readr)
+library(dplyr)
 library(tidyr)
 library(stringr)
-library(osfr)
 
-qualtrics_file <- "test_weightings.csv"
+qualtrics_file <- "https://osf.io/kxbmc/download"
+
 initial_frame <- read_csv(qualtrics_file, col_names = TRUE, show_col_types = FALSE) %>%
   select(starts_with("Q")) %>%
   filter(!row_number() %in% c(2)) %>%
@@ -11,8 +13,5 @@ initial_frame <- read_csv(qualtrics_file, col_names = TRUE, show_col_types = FAL
   separate_wider_delim(" - ", names = c("A", "B"), cols = "V1") %>%
   mutate(A = str_replace_all(A, "Civic Learning.", "Civic Learning:")) %>%
   mutate(A = str_replace_all(A, "Which of the following best describes the nature of the goals achieved in your project?", "Nature of Goals: Which of the following best describes the nature of the goals achieved in your project?")) %>%
-  mutate(A = str_split(initial_frame$A, ": ", simplify = TRUE)[,1])
+  mutate(A = str_split(A, ": ", simplify = TRUE)[,1])
 
-reduced_prompt <- str_split(initial_frame$A, ": ", simplify = TRUE)[,1]
-
-initial_frame$A <- reduced_prompt                       
